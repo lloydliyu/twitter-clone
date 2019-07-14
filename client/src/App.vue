09efa68b1bb1d5e723663c555f17bc5723bdc374
Daniel Carter <daniel.carter@intercom.io>
2019-07-14 13:25:35 +0100

    correctly align avatar and fix bug with avatar during login.

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,20 +9,21 @@
         <v-btn :to="{ name: 'login' }" flat><span>Login</span></v-btn>
         <v-btn :to="{ name: 'signup' }" flat><span>Sign Up</span></v-btn>
       </v-toolbar-items>
-      <v-toolbar-items v-if="user.user">
-        <v-avatar color="grey darken-3" v-if="user.user.imageUrl">
-          <img
-            class="elevation-6"
-            :src="post.author.imageUrl"
-          />
-        </v-avatar>
-        <v-avatar v-if="!user.user.imageUrl" color="red" size=40>
-          <span class="white--text headline">{{ user.user.displayName[0].toUpperCase() }}</span>
-        </v-avatar>
+      <v-toolbar-items v-if="user">
+        <v-layout justify-content align-center v-if="user.user">
+          <v-avatar color="grey darken-3" v-if="user.user.imageUrl">
+            <img
+              class="elevation-6"
+              :src="user.user.imageUrl"
+            />
+          </v-avatar>
+          <v-avatar v-if="!user.user.imageUrl" color="red" size=40>
+            <span class="white--text headline">{{ user.user.displayName[0].toUpperCase() }}</span>
+          </v-avatar>
+        </v-layout>
         <v-btn flat @click="logout"><span>Logout</span></v-btn>
       </v-toolbar-items>
     </v-toolbar>
-    
     <v-content>
       <router-view/>
     </v-content>
